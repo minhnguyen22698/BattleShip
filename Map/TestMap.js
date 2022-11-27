38dd58fccd7327d7d5e8ea13354fa7a3a1a70e73
LongMiu <thlong206@gmail.com>
2022-11-28 01:14:11 +0700

    Add collision

--- Map/TestMap.js
+++ Map/TestMap.js
@@ -1,9 +1,14 @@
-
+const COLLISION_TYPE = {
+    CIRCLE: 0,
+    SQUARES: 1,
+    POLYGON: 2
+}
 cc.Class({
     extends: cc.Component,
 
     properties: {
         mapTmx: cc.TiledMap,
+        colliderZonePrefab: cc.Prefab,
         _listColliderLayer: [],
         _layerCollidable: null
     },
@@ -13,40 +18,55 @@ cc.Class({
         manager.enabled = true;
         manager.enabledDebugDraw = true;
         
+        this.setupCollidableLayer();
+    },
+
+    setupCollidableLayer(){
         this._layerCollidable = this.mapTmx.getObjectGroup("ColliderObject")
-        
-        // cc.warn(this.mapTmx);
-        // cc.warn(this._layerCollidable);
+        if(!this._layerCollidable){
+            cc.error("No Layer Collidable");
+            return;
+        }
+        this._layerCollidable.node.anchorX = 0;
+        this._layerCollidable.node.anchorY = 1;
+        const posX = -this._layerCollidable.node.width / 2;
+        const posY = this._layerCollidable.node.height / 2;
+        this._layerCollidable.node.setPosition(posX, posY);
+        this.createCollision();
     },
 
-    start(){
-        this.getCollidableLayer();
-    },
-
-    getCollidableLayer(){
+    createCollision(){
         const listCollisions = this._layerCollidable.getObjects();
         for(let i = 0; i < listCollisions.length; i++){
-            this.createCollision(listCollisions[i]);
+            switch(listCollisions[i].type){
+                case COLLISION_TYPE.CIRCLE:
+                    this.createCircleCollider(listCollisions[i]);
+                    break;
+                case COLLISION_TYPE.SQUARES:
+                    this.createSquaresCollider(listCollisions[i]);
+                    break;
+                case COLLISION_TYPE.POLYGON:
+                    this.createPolygonCollider(listCollisions[i]);
+                    break;
+            }
         }
     },
 
-    createCollision(object){
-        cc.warn("object:", object)
+    createPolygonCollider(object){
         const listPoints = object.points;
-        const listWorldPoints = [];
-        const listNodePoints = [];
-        for(let i = 0; i < listPoints.length; i++){
-            listPoints[i].z = 0;
-            const newPoint = this.node.convertToWorldSpaceAR(listPoints[i]);
-            listWorldPoints.push(newPoint);
-        }
-        for(let i = 0; i < listWorldPoints.length; i++){
-            const newPoint = this._layerCollidable.node.convertToNodeSpaceAR(listPoints[i]);
-            listNodePoints.push(newPoint);
-        }
-        const colliderComp = this._layerCollidable.node.addComponent(cc.PolygonCollider);
-        colliderComp.points = listNodePoints;
-        cc.warn("listWorldPoints:", listNodePoints);
-    }
+        const nodeCollider = cc.instantiate(this.colliderZonePrefab);
+        this._layerCollidable.node.addChild(nodeCollider);
+        nodeCollider.setPosition(cc.v2(object.offset.x, -object.offset.y));
+        const colliderComp = nodeCollider.addComponent(cc.PolygonCollider);
+        colliderComp.points = listPoints;
+    },
+
+    createSquaresCollider(object){
+        cc.warn(object);
+    },
+
+    createCircleCollider(object){
+        cc.warn(object);
+    },
 
 });
