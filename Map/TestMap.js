77a9cd24d3131cdf3c3615747f92417c1f143da2
LongMiu <thlong206@gmail.com>
2022-12-11 11:42:44 +0700

    no message

--- Map/TestMap.js
+++ Map/TestMap.js
@@ -2,7 +2,7 @@ const COLLISION_TYPE = {
     CIRCLE: 0,
     SQUARES: 1,
     POLYGON: 2
-}
+};
 cc.Class({
     extends: cc.Component,
 
@@ -15,14 +15,19 @@ cc.Class({
 
     onLoad(){
         const manager = cc.director.getCollisionManager();
+        const managerPhysic = cc.director.getPhysicsManager();
+        managerPhysic.gravity = cc.v2 (0, 0);
+        managerPhysic.enabled = true;
+        managerPhysic.debugDrawFlags = 1;
         manager.enabled = true;
+        manager.enabledDrawBoundingBox = true;
         manager.enabledDebugDraw = true;
-        
+
         this.setupCollidableLayer();
     },
 
     setupCollidableLayer(){
-        this._layerCollidable = this.mapTmx.getObjectGroup("ColliderObject")
+        this._layerCollidable = this.mapTmx.getObjectGroup("ColliderObject");
         if(!this._layerCollidable){
             cc.error("No Layer Collidable");
             return;
@@ -32,6 +37,10 @@ cc.Class({
         const posX = -this._layerCollidable.node.width / 2;
         const posY = this._layerCollidable.node.height / 2;
         this._layerCollidable.node.setPosition(posX, posY);
+        
+    },
+
+    start(){
         this.createCollision();
     },
 
@@ -53,20 +62,35 @@ cc.Class({
     },
 
     createPolygonCollider(object){
-        const listPoints = object.points;
+        cc.log("::Create Collider Polygon::", object);
+        const listPoints = [...object.points];
         const nodeCollider = cc.instantiate(this.colliderZonePrefab);
+        const colliderComp = nodeCollider.addComponent(cc.PhysicsPolygonCollider);
+        const RBcomp = nodeCollider.getComponent(cc.RigidBody);
         this._layerCollidable.node.addChild(nodeCollider);
         nodeCollider.setPosition(cc.v2(object.offset.x, -object.offset.y));
-        const colliderComp = nodeCollider.addComponent(cc.PolygonCollider);
+        RBcomp.type = 0;
         colliderComp.points = listPoints;
+        // colliderComp.apply();
+        colliderComp.apply(colliderComp.points, listPoints);
+        // colliderComp.apply(listPoints, colliderComp.points);
+
+        // const nodeColliderTest = cc.instantiate(this.colliderZonePrefab);
+        // const colliderCompTest = nodeCollider.addComponent(cc.PolygonCollider);
+        // this._layerCollidable.node.addChild(nodeColliderTest);
+        // nodeColliderTest.setPosition(cc.v2(object.offset.x, -object.offset.y));
+        // colliderCompTest.points = listPoints;
+
+        cc.error(colliderComp);
+
     },
 
     createSquaresCollider(object){
-        cc.warn(object);
+        cc.log("::Create Collider Squares::", object);
     },
 
     createCircleCollider(object){
-        cc.warn(object);
+        cc.log("::Create Collider Circle::", object);
     },
 
 });
